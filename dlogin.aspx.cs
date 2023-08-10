653bbbf6786a3fa5c9fa4371d39a73bc6b90ca5d
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2023-08-10 15:47:12 +0530

    Add files via upload

---
+++ dlogin.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+
+public partial class dlogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+        string str = WebConfigurationManager.ConnectionStrings["cn"].ToString();
+        SqlConnection cn = new SqlConnection(str);
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("select * from doctor where username=@username and password=@password", cn);
+        cmd.Parameters.AddWithValue("@username", unm.Text);
+        cmd.Parameters.AddWithValue("@password  ", pass.Text);
+        SqlDataAdapter da  = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        int i = cmd.ExecuteNonQuery();
+        cn.Close();
+        if (dt.Rows.Count > 0)
+        {
+            Session["id"] = unm.Text;
+            Response.Redirect("~/doctor/profile.aspx");
+            Session.RemoveAll();
+           
+        }
+        else
+        {
+            Response.Write("");
+        }  
+    }
+    protected void forgotpass_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("dforgotpass.aspx");
+    }
+}
